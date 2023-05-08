81298e6ad97eafaeb045c92a13a39e888c22f08a
Leon Arndt <leon.arndt@innogames.com>
2023-05-08 19:52:56 +0200

    add blinking animation

---
+++ Assets/Scripts/UserInterface/InventoryItem.cs
@@ -0,0 +1,18 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UserInterface
+{
+	public class InventoryItem : MonoBehaviour
+	{
+		[SerializeField] private TextMeshProUGUI amount;
+		[SerializeField] private Image icon;
+
+		public void Set(Sprite sprite, uint newAmount)
+		{
+			icon.sprite = sprite;
+			amount.text = newAmount.ToString();
+		}
+	}
+}
