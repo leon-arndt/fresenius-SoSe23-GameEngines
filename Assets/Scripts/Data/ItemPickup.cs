9ed0541d0d1109dc5c28c2614e3908000a1ed3e2
Leon Arndt <leon.arndt@innogames.com>
2023-05-15 18:41:24 +0200

    WIP pooling

--- Assets/Scripts/Data/ItemPickup.cs
+++ Assets/Scripts/Data/ItemPickup.cs
@@ -6,6 +6,8 @@ namespace Data
 	[CreateAssetMenu(fileName = "New ItemPickup", menuName = "Items/ItemPickup")]
 	public class ItemPickup : InstantPickup
 	{
+		public Sprite icon;
+		
 		public override void OnPickup()
 		{
 			ScriptableObjectServiceLocator.Get<InventoryService>().Add(this);
--- Assets/Scripts/UserInterface/InventoryItem.cs
+++ Assets/Scripts/UserInterface/InventoryItem.cs
@@ -1,18 +1,20 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.Pool;
 using UnityEngine.UI;
 
 namespace UserInterface
 {
-	public class InventoryItem : MonoBehaviour
-	{
-		[SerializeField] private TextMeshProUGUI amount;
-		[SerializeField] private Image icon;
+    public class InventoryItem : MonoBehaviour
 
-		public void Set(Sprite sprite, uint newAmount)
-		{
-			icon.sprite = sprite;
-			amount.text = newAmount.ToString();
-		}
-	}
+    {
+        [SerializeField] private TextMeshProUGUI amount;
+        [SerializeField] private Image icon;
+
+        public void Set(Sprite sprite, uint newAmount)
+        {
+            icon.sprite = sprite;
+            amount.text = newAmount.ToString();
+        }
+    }
 }
