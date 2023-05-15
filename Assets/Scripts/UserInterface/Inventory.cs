2c7f3d2ed21ff7b12c0e2ab04161c9e5fc67d52b
Leon Arndt <leon.arndt@innogames.com>
2023-05-15 18:43:45 +0200

    naming

--- Assets/Scripts/UserInterface/Inventory.cs
+++ Assets/Scripts/UserInterface/Inventory.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
 using UnityEngine.Pool;
+using UnityEngine.Serialization;
 using UserInterface;
 
 public class Inventory : MonoBehaviour
 {
     // stack-based ObjectPool available with Unity 2021 and above
-    private IObjectPool<RevisedProjectile> objectPool;
+    private IObjectPool<InventoryItem> objectPool;
 
     // throw an exception if we try to return an existing item, already in the pool
     [SerializeField] private bool collectionCheck = true;
@@ -13,37 +14,37 @@ public class Inventory : MonoBehaviour
     // extra options to control the pool capacity and maximum size
     [SerializeField] private int defaultCapacity = 20;
     [SerializeField] private int maxSize = 100;
-    [SerializeField] RevisedProjectile projectilePrefab;
+    [FormerlySerializedAs("projectilePrefab")] [SerializeField] InventoryItem inventoryItemPrefab;
 
     private void Awake()
     {
-        objectPool = new ObjectPool<RevisedProjectile>(CreateProjectile,
+        objectPool = new ObjectPool<InventoryItem>(CreatePooledItem,
             OnGetFromPool, OnReleaseToPool, OnDestroyPooledObject,
             collectionCheck, defaultCapacity, maxSize);
     }
 
     // invoked when creating an item to populate the object pool
-    private RevisedProjectile CreateProjectile()
+    private InventoryItem CreatePooledItem()
     {
-        RevisedProjectile projectileInstance = Instantiate(projectilePrefab);
-        projectileInstance.ObjectPool = objectPool;
-        return projectileInstance;
+        InventoryItem poolInstance = Instantiate(inventoryItemPrefab);
+        poolInstance.ObjectPool = objectPool;
+        return poolInstance;
     }
 
     // invoked when returning an item to the object pool
-    private void OnReleaseToPool(RevisedProjectile pooledObject)
+    private void OnReleaseToPool(InventoryItem pooledObject)
     {
         pooledObject.gameObject.SetActive(false);
     }
 
     // invoked when retrieving the next item from the object pool
-    private void OnGetFromPool(RevisedProjectile pooledObject)
+    private void OnGetFromPool(InventoryItem pooledObject)
     {
         pooledObject.gameObject.SetActive(true);
     }
 
     // invoked when we exceed the maximum number of pooled items (i.e. destroy the pooled object)
-    private void OnDestroyPooledObject(RevisedProjectile pooledObject)
+    private void OnDestroyPooledObject(InventoryItem pooledObject)
     {
         Destroy(pooledObject.gameObject);
     }
--- Assets/Scripts/UserInterface/InventoryItem.cs
+++ Assets/Scripts/UserInterface/InventoryItem.cs
@@ -6,15 +6,27 @@ using UnityEngine.UI;
 namespace UserInterface
 {
     public class InventoryItem : MonoBehaviour
-
     {
         [SerializeField] private TextMeshProUGUI amount;
         [SerializeField] private Image icon;
 
+        // deactivate after delay
+        [SerializeField] private float timeoutDelay = 3f;
+
+        private IObjectPool<InventoryItem> objectPool;
+        
+        public IObjectPool<InventoryItem> ObjectPool { set => objectPool = value; }
+        
         public void Set(Sprite sprite, uint newAmount)
         {
             icon.sprite = sprite;
             amount.text = newAmount.ToString();
         }
+
+        public void Deactivate()
+        {
+            gameObject.SetActive(false);
+        }
+        
     }
 }
