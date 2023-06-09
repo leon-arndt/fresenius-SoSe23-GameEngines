64e50b564f6aaa293d96003ba376a827b32a6272
Leon Arndt <leon.arndt@innogames.com>
2023-06-09 15:44:13 +0800

    fade in and out the dialog background

--- Assets/Scripts/UserInterface/DialogUi.cs
+++ Assets/Scripts/UserInterface/DialogUi.cs
@@ -8,14 +8,18 @@ namespace UserInterface
     public class DialogUi : MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI dialogText;
+        [SerializeField] private CanvasGroup canvasGroup;
 
         private void Start()
         {
             gameObject.SetActive(false);
+            canvasGroup.alpha = 0f;
         }
 
-        public void Set(Speaker speakerName, string dialog)
+        public void Set(Speaker speakerName, string dialog, AudioClip dialogDataAudioClip)
         {
+            gameObject.SetActive(true);
+            canvasGroup.DOFade(1f, 1f).From(0f);
             var charactersPerSecond = 30f;
             var animationTime = dialog.Length / charactersPerSecond;
             DOVirtual.Float(
@@ -28,8 +32,7 @@ namespace UserInterface
                     dialogText.text = $"<color=#ffff00> {speakerName.speakerName} </color> {dialogSubText}";
                 });
             
-            gameObject.SetActive(true);
-            
+            canvasGroup.DOFade(0f, 1f).SetDelay(dialogDataAudioClip.length * 1.1f);
         }
     }
 }
